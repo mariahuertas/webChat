7058982eaaad64a198f421b7a0b2dcbddb51e080
maria <mariahuertas296@gmail.com>
2020-01-21 08:28:33 +0100

    Remove unused imports in ChatManager and Chat clasess

--- src/main/java/es/codeurjc/webchat/ChatManager.java
+++ src/main/java/es/codeurjc/webchat/ChatManager.java
@@ -2,8 +2,6 @@ package es.codeurjc.webchat;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
