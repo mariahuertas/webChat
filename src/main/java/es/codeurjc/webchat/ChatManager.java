b589bef37f68b685fcc45bf7931e7c6baefa5f27
maria <mariahuertas296@gmail.com>
2020-02-02 16:28:50 +0100

    Add maximum wait time when creating the chat - code (New functionality)

--- src/main/java/es/codeurjc/webchat/ChatManager.java
+++ src/main/java/es/codeurjc/webchat/ChatManager.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -12,9 +13,11 @@ public class ChatManager {
 	private ConcurrentMap<String, Chat> chats = new ConcurrentHashMap<>();
 	private ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
 	private int maxChats;
+	private Semaphore chatAvailable;
 
 	public ChatManager(int maxChats) {
 		this.maxChats = maxChats;
+		this.chatAvailable = new Semaphore(this.maxChats);
 	}
 
 	public void newUser(User user) {
@@ -28,8 +31,8 @@ public class ChatManager {
 	}
 
 	public Chat newChat(String name, long timeout, TimeUnit unit) throws InterruptedException,
-			TimeoutException {
-
+	TimeoutException {
+	
 		if (chats.size() == maxChats) {
 			throw new TimeoutException("There is no enought capacity to create a new chat");
 		}
@@ -47,9 +50,34 @@ public class ChatManager {
 			return newChat;
 		}
 	}
+	
+	public Chat newChatWithTimeOut(String name, long timeout, TimeUnit unit) throws InterruptedException, TimeoutException {
+
+		try {
+			if (!chatAvailable.tryAcquire(timeout, unit)) {
+				throw new TimeoutException("There is no enought capacity to create a new chat");
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		if (chats.containsKey(name)) {
+			return chats.get(name);
+		} else {
+			Chat newChat = new Chat(this, name);
+			chats.putIfAbsent(name, newChat);
+
+			for (User user : users.values()) {
+				user.newChat(newChat);
+			}
+
+			return newChat;
+		}
+	}
 
 	public void closeChat(Chat chat) {
 		Chat removedChat = chats.remove(chat.getName());
+		
 		if (removedChat == null) {
 			throw new IllegalArgumentException("Trying to remove an unknown chat with name \'"
 					+ chat.getName() + "\'");
@@ -58,6 +86,7 @@ public class ChatManager {
 		for(User user : users.values()){
 			user.chatClosed(removedChat);
 		}
+		chatAvailable.release();
 	}
 
 	public Collection<Chat> getChats() {
